b00a4f10e95678e60acc81fd9899918589a33c1d
carbondesigned <dylan.reeder5@gmail.com>
2021-06-06 10:26:46 -0400

    added font

--- src/App.js
+++ src/App.js
@@ -43,8 +43,6 @@ const App = () => {
   const songEndHandler = async () => {
     let currentIndex = songs.findIndex((song) => song.id === currentSong.id)
     await setCurrentSong(songs[(currentIndex + 1) % songs.length])
-
-    if (isPlaying) audioRef.current.play()
   }
 
   return (
